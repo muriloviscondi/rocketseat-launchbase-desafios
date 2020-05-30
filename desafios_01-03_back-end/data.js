71cc724a2ebf953110b5b71a6bda16f549d66211
muriloviscondi <muriloviscondi@gmail.com>
2020-05-29 22:09:22 -0300

    finalizando o nunjuks

---
+++ desafios_01-03_back-end/data.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    id: 'starter',
+    url: 'https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg',
+    url_description: 'Logo curso Rocketseat Starter',
+    info: '<strong class="color-starter">Torne-se um programador desejado</strong>no mercado com esses cursos.',
+    price: '<strong>Free</strong>'
+  },
+  {
+    id: 'launchbase',
+    url: 'https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg',
+    url_description: 'Logo curso Rocketseat LaunchBase',
+    info: '<strong class="color-launchbase">Domine programação do zero</strong> e tenha acesso às melhores oportunidades do mercado.',
+    price: '<strong>Garanta sua vaga</strong>para a próxima turma'
+  },
+  {
+    id: 'gostack',
+    url: 'https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg',
+    url_description: 'Logo curso Rocketseat GoStack',
+    info: '<strong class="color-gostack">Treinamento imersivo</strong> nas tecnologias mais modernas de desenvolvimento web e mobile.',
+    price: '<strong>Garanta sua vaga</strong> para a próxima turma'
+  }
+]
--- desafios_01-03_back-end/server.js
+++ desafios_01-03_back-end/server.js
@@ -2,27 +2,74 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const courses = require('./data')
 
 server.use(express.static('public'))
 
 server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
-  express: server
+  express: server,
+  autoescape: false
 })
 
-server.get('/', function(request, response) {
+server.get('/', function (request, response) {
   return response.render('home')
 })
 
-server.get('/about', function(request, response) {
-  return response.render('about')
+server.get('/about', function (request, response) {
+  const about = {
+    url_img: 'https://pbs.twimg.com/profile_images/953595371875422210/0pWsfSSp_400x400.jpg',
+    url_description: 'Logo Rocketseat',
+    title: 'Rocketseat',
+    title_description: 'As melhores tecnologias em programação, direto ao ponto e do jeito certo.',
+    description: 'Imagine você dominando as mesmas tecnologias adotadas pelos melhores times do mundo, utilizando como por exemplo:',
+    list_techs: [
+      {
+        name: 'Javascript'
+      },
+      {
+        name: 'ReactJS'
+      },
+      {
+        name: 'React Native'
+      },
+      {
+        name: 'NodeJs'
+      },
+    ],
+    description_tech: 'Venha fazer parte dessa comunidade crescente chamada <a href="https://rocketseat.com.br/">Rocketseat</a>.',
+    links: [
+      {
+        name: 'Github',
+        url: 'https://github.com/Rocketseat'
+      },
+      {
+        name: 'YouTube',
+        url: 'https://www.youtube.com/channel/UCSfwM5u0Kce6Cce8_S72olg'
+      },
+      {
+        name: 'Instagram',
+        url: 'https://www.instagram.com/rocketseat_oficial/?hl=en'
+      },
+      {
+        name: 'Twitter',
+        url: 'https://twitter.com/rocketseat'
+      },
+      {
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/school/rocketseat'
+      }
+    ]
+  }
+
+  return response.render('about', { about })
 })
 
-server.get('/content', function(request, response) {
-  return response.render('content')
+server.get('/content', function (request, response) {
+  return response.render('content', { items: courses })
 })
 
-server.listen(5000, function() {
+server.listen(5000, function () {
   console.log('server is running!')
 })
